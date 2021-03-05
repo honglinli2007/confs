1131358ef9c76531ecd27dcf7738c5267559de17
lennychen <lennychen@tencent.com>
2021-03-05 15:11:13 +0800

    offline install plug.vim

--- vim-backup/conf-install.sh
+++ vim-backup/conf-install.sh
@@ -3,6 +3,12 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+if [ -z $(find ${XDG_DATA_HOME:-$HOME/.local/share}/nvim -name plug.vim) ]
+then
+	cp ./plug.vim ${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim
+fi
+
 mkdir -p ~/.config/nvim/ || true
-ln -s $(readlink -f init.vim) ~/.config/nvim/init.vim || true
-echo Run PluginInstall in vim
+ln -s $(readlink -f init.vim) ~/.config/nvim/init.vim > /dev/null 2>&1|| echo ~/.config/nvim/init.vim exists
+echo Now please run PluginInstall in vim
+
