1d4dda26ac572b0e15cd31bb98345e1fa4be17d2
Lei Chen <lennychen@tencent.com>
2021-03-05 11:12:47 +0800

    update nvim preinstall.sh

--- vim-backup/preinstall.sh
+++ vim-backup/preinstall.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 ./nvim.appimage --appimage-extract
+
+target_dir=/opt/nvim
+if [ -d $target_dir ]
+then
+	echo $target_dir exists, Failed!!!!
+	exit 0
+fi
+
+mv squashfs-root/ /opt/nvim
