5ebcbf994548369857965a26b3c3fa365f6cba4c
lennychen <lennychen@tencent.com>
2021-04-09 17:45:32 +0800

    git: disable branch pager

--- git/git-conf.sh
+++ git/git-conf.sh
@@ -1,3 +1,4 @@
 git config --global user.name  "lennychen"
 git config --global user.email "lennychen@tencent.com"
 git config --global credential.helper store
+git config --global pager.branch false
