abcdda1a83059fcf5022ad343ca9ed7dc76e5aec
lennychen <lennychen@tencent.com>
2021-04-10 18:42:54 +0800

    touch ~/.rpmmacros

--- kernel/install.sh
+++ kernel/install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 mkdir -p /data/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS} || true
+touch /data/.rpmmacros || true
 yum install asciidoc audit-libs-devel binutils-devel elfutils-devel newt-devel numactl-devel openssl-devel python2-devel xmlto
 
